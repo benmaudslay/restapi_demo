71d0f6d88947938afbd1a93a4150bcd18f71bae0
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro.local>
2021-03-31 11:47:30 +0100

    added user model & user patch + delete routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 require("./db/connection");
 const express = require("express");
+const { User } = require("./models/User");
 
 const port = process.env.PORT || 5000;
 // init instance of express
 const app = express();
+
+// middleware
+app.use(express.json());
 
 // routes/endpoints
 app.get("/health", (req, res) => {
@@ -13,19 +17,52 @@ app.get("/health", (req, res) => {
 app.get("/user", async (req, res) => {
   try {
     const allUsers = await User.find({});
-    res.send(allUsers);
+    res.status(200).send(allUsers);
   } catch (error) {
-    res.send(error);
+    res.status(500).send(error);
   }
 });
 
 app.post("/user", async (req, res) => {
+  // this is the route to add a user
   try {
     const user = new User(req.body);
+    // ^ this is the same as:
+    // const user = new User({
+    //   name: req.body.name,
+    //   email: req.body.email,
+    //   password: req.body.password,
+    // });
     const returnedValue = await user.save();
-    res.send(`Successfully added ${returnedValue.name}`);
+    res.status(201).send(`Successfully added ${returnedValue.name}`);
   } catch (error) {
-    res.send(error);
+    res.status(400).send(error);
+  }
+});
+
+app.patch("/user/:id", async (req, res) => {
+  // this will be our route to update a user
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    console.log(user);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(404).send({ message: "user not found" });
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  // this will be our route to delete a user
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.status(200).send(user);
+    // If you wanted to use promises instead of async:
+    // User.findByIdAndDelete(req.params.id)
+    //   .then((user) => res.send(user))
+  } catch (error) {
+    res.status(404).send({ message: "user not found" });
   }
 });
 
